d6fa717da2cc7b698bdada7ecb3720b75b1e46b3
shafaalafghany <shafagabriel1@gmail.com>
2020-04-18 12:40:02 +0700

    Merubah nama controller

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller
+class auth extends CI_Controller
 {
     public function __construct()
     {
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller
+class home extends CI_Controller
 {
     public function __construct()
     {
